6476e66adf88332ca563f98be6f65e4ae88eb1dd
zvakanaka <quintonish@gmail.com>
2020-01-28 00:15:21 -0700

    test it

--- docs/precache-manifest.a00200c0d0acf76b08bb24509239474c.js
+++ docs/precache-manifest.a00200c0d0acf76b08bb24509239474c.js
@@ -5,14 +5,14 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   },
   {
     "revision": "0f206f4785767c4c3864",
-    "url": "/static/js/2.94c345a9.chunk.js"
+    "url": "/scriptures-of-Jesus/static/js/2.94c345a9.chunk.js"
   },
   {
     "revision": "00b95452eab107ecf71f",
-    "url": "/static/js/main.dd397781.chunk.js"
+    "url": "/scriptures-of-Jesus/static/js/main.dd397781.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
-    "url": "/static/js/runtime~main.a8a9905a.js"
+    "url": "/scriptures-of-Jesus/static/js/runtime~main.a8a9905a.js"
   }
 ]);
