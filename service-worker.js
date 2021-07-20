464df258c27e57231f53932c56dea3d06af62cf2
Adam <8365885+zvakanaka@users.noreply.github.com>
2021-07-19 23:55:48 -0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/scriptures-of-Jesus/precache-manifest.b56c45965830532efa5c20efeb10f5ce.js"
+  "/scriptures-of-Jesus/precache-manifest.7ace570e114b69210d2cbe859cc3dcfc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/scriptures-of-Jesus/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
